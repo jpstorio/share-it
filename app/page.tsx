38ec7068ea24a4120a14186aec048f6b61cba30c
Torio <jpstorio@mymail.mapua.edu.ph>
2023-04-11 21:05:50 +0800

    add addPost component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import AddPost from "./components/AddPost";
+
 export default function Home() {
     return (
         <main>
             <h1>Hello World</h1>
+            <AddPost />
         </main>
     );
 }
