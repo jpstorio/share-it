fa513a1c8bc1163ff53f665a56313ae70d020a04
Torio <jpstorio@mymail.mapua.edu.ph>
2023-04-11 23:02:06 +0800

    feat addPost api call

---
+++ pages/api/posts/addPost.tsx
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+import prisma from "../../../prisma/client";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "POST") {
+        const session = await getServerSession(req, res, authOptions);
+        if (!session) {
+            return res.status(401).json({ message: "Please sign in to make a post." });
+        }
+
+        const title: string = req.body.title;
+
+        //Get User
+        const prismaUser = await prisma.user.findUnique({
+            where: { email: session?.user?.email },
+        });
+
+        //Check title
+        if (title.length > 300) {
+            return res.status(403).json({ message: "Please write a shorter post" });
+        }
+        if (!title.length) {
+            console.log("empty");
+            return res.status(403).json({ message: "Please write something before we can post it." });
+        }
+
+        //Create Post
+        try {
+            const result = await prisma.post.create({
+                data: {
+                    title,
+                    userId: prismaUser.id,
+                },
+            });
+            res.status(200).json(result);
+        } catch (err) {
+            res.status(403).json({ err: "Error has occured while making a post" });
+        }
+    }
+}
