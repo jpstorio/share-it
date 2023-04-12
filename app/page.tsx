74448998e20fe81e27ae856827b81f39d95d3034
Torio <jpstorio@mymail.mapua.edu.ph>
2023-04-13 00:19:37 +0800

    add home posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
 "use client";
+import axios from "axios";
 import AddPost from "./components/AddPost";
+import { useQuery } from "@tanstack/react-query";
+import Post from "./components/Post";
+import { PostsType } from "./types/Posts";
+
+//Fetch posts
+const allPosts = async () => {
+    const response = await axios.get("/api/posts/getPosts");
+    return response.data;
+};
 
 export default function Home() {
+    const { data, error, isLoading } = useQuery<PostsType[]>({ queryFn: allPosts, queryKey: ["posts"] });
+    if (error) {
+        return error;
+    }
+    if (isLoading) {
+        return "Loading..";
+    }
+    console.log(data);
     return (
         <main>
-            <h1>Hello World</h1>
             <AddPost />
+            {data?.map((post) => {
+                return (
+                    <Post
+                        comments={post.Comment}
+                        key={post.id}
+                        name={post.user.name}
+                        avatar={post.user.image}
+                        postTitle={post.title}
+                        id={post.id}
+                    />
+                );
+            })}
         </main>
     );
 }
